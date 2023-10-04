824a8738ed49c5f9f2e31418363cd9fc2ca8c9a7
Elsa Ruiz Fernandez <erfrndz@outlook.fr>
2023-10-04 18:48:49 +0200

    Background color ajoutées sur toutes les pages via css

--- wp-content/themes/PLANTY/functions.php
+++ wp-content/themes/PLANTY/functions.php
@@ -3,5 +3,13 @@
 add_action ('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles ()
 {
-    wp_enqueue_style ('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style ('child-theme-css', get_stylesheet_directory_uri() . '/style.css');
 }
+
+function ajouter_menu_admin_si_connecte($items, $args) {
+    if (is_user_logged_in() && $args->theme_location == 'Menu-principal') {
+        $items .= '<li><a href="' . esc_url(home_url('/admin')) . '">Admin</a></li>';
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'ajouter_menu_admin_si_connecte', 10, 2);
